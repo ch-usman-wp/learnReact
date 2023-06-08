c2ca830be05696eecc3ffcf2b30eef6e01500989
ch-usman-wp <chusman924@gmail.com>
2023-06-09 00:13:05 +0500

    use state , UseEffect and useReff

--- learning-react/src/App.js
+++ learning-react/src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {useState, useEffect, useRef} from 'react';
 
 function App() {
+ const [count, setcounter] = useState(0)
+ const [data, SetData] = useState('ram')
+ function updateCount(){
+  setcounter(count + 1)
+ }
+ function updateData(){
+  SetData('usman')
+ }
+//  use effect 
+    useEffect(()=>{
+      console.log("Component mountend")
+    }, [data])
+
+    // what is useRef ? dom manipulate
+    const refElement = useRef("")
+    console.log(refElement)
+    const [name, setName] = useState("Usman");
+    function resetName(){
+      setName("");
+      refElement.current.focus()
+    }
+    function colorChange(){
+      refElement.current.style.color="red"
+    }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Button Click {count}</h1>
+      <button onClick={updateCount}>click me </button>
+      <button onClick={updateData}>click me </button>
+      <br/>
+      <input ref={refElement} type="text" value={name} onChange={(e)=>setName(e.target.value)} />
+      <button onClick={resetName}>reset</button>
+      <button onClick={colorChange}>Color Chnage</button>
+
+    </>
   );
 }
 
